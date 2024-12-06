c069a7a4cd0856a76a37a2fb1a507e4fc55d9578
Gustavo <glinares@unsa.edu.pe>
2024-12-06 05:43:14 -0500

    Solucionando Eliminar.pl

--- cgi-bin/eliminar.pl
+++ cgi-bin/eliminar.pl
@@ -7,6 +7,7 @@ use CGI qw(:standard);
 my $data_dir = "/var/www/html/data/";
 
 my $pagina = param('pagina');
+my $mensaje = '';  # Variable para almacenar el mensaje
 
 print header(-type => 'text/html', -charset => 'UTF-8');
 
@@ -14,16 +15,16 @@ if ($pagina && $pagina =~ /^[a-zA-Z0-9_.-]+\.md$/) {
     my $file_path = $data_dir . $pagina;
     if (-e $file_path) {
         unlink($file_path) or die "No se pudo eliminar la página: $!";
-        print redirect('listado.pl');
+        $mensaje = "La página '$pagina' ha sido eliminada correctamente.";  # Mensaje de éxito
     } else {
-        print start_html('Error');
-        print "<h1>Error: Página no encontrada.</h1>";
-        print "<a href='listado.pl'>Volver al Listado</a>";
-        print end_html;
+        $mensaje = "Error: Página no encontrada.";  # Mensaje de error si no se encuentra el archivo
     }
 } else {
-    print start_html('Error');
-    print "<h1>Error: Parámetro inválido.</h1>";
-    print "<a href='listado.pl'>Volver al Listado</a>";
-    print end_html;
+    $mensaje = "Error: Parámetro inválido.";  # Mensaje de error si el parámetro no es válido
 }
+
+# Mostrar la página HTML con el mensaje y el enlace
+print start_html('Resultado de la operación');
+print "<h1>$mensaje</h1>";
+print "<a href='listado.pl'>Volver al Listado</a>";
+print end_html;
