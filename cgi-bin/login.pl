8e4a1f277533f719ca849ea1016423f9975f9823
jigreda <jigreda@unsa.edu.pe>
2024-12-15 09:37:39 -0500

    Corrigiendo el login

--- cgi-bin/login.pl
+++ cgi-bin/login.pl
@@ -6,18 +6,14 @@ use DBI;
 
 # Conexión a la base de datos
 my $dbh = DBI->connect('DBI:mysql:database=tu_base;host=localhost', 'usuario', 'contraseña', { RaiseError => 1, AutoCommit => 1 });
-
 my $user = param('user');
 my $password = param('password');
-
 print header(-type => 'application/xml', -charset => 'UTF-8');
 print "<?xml version='1.0' encoding='utf-8'?>\n";
-
-if ($user && $password) {
+if ($user && $password){
     my $sth = $dbh->prepare("SELECT userName, firstName, lastName FROM Users WHERE userName = ? AND password = ?");
     $sth->execute($user, $password);
-
-    if (my $row = $sth->fetchrow_hashref) {
+    if (my $row = $sth->fetchrow_hashref){
         print "<user>\n";
         print "  <owner>$row->{userName}</owner>\n";
         print "  <firstName>$row->{firstName}</firstName>\n";
@@ -30,5 +26,4 @@ if ($user && $password) {
 } else {
     print "<user></user>\n";
 }
-
 $dbh->disconnect;
